a08268a4ae6ffbda68bcff1dc82b6a214105cba3
khaliev <dev.adlan@icloud.com>
2026-05-16 23:53:03 +0200

    adding js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Select we're selecting DOM elements
+const newTaskInput = document.getElementById("new-task");
+const addTaskButton = document.querySelector("button");
+const taskList = document.getElementById("task-list");
